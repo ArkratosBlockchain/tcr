d7d52ac4faa964f07b6ebbe056c76d899a103323
Sue Kah Yong <skybach@gmail.com>
2018-12-13 09:35:30 +0800

    update network config

--- truffle.js
+++ truffle.js
@@ -21,10 +21,12 @@ module.exports = {
       gasPrice: 10000000000,
     },
     ganache: {
-      provider: () => new HDWalletProvider(mnemonic, 'http://localhost:8545'),
+      host: 'localhost',
+      port: 7545,
+      // provider: () => new HDWalletProvider(mnemonic, 'http://localhost:7545'),
       network_id: '*',
       gas: 6000000,
-      gasPrice: 25000000000,
+      gasPrice: 100000000,
     },
     rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io'),
